ba663548793f242d2d18d23ba6f4b9cb8cdedd23
Ishant Kumar <ishant010301@gmail.com>
2025-03-12 00:31:49 +0530

    CHECK 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
 "use client";
 import { motion } from "framer-motion";
-import QuotesCarousel from "@/components/ui/Home/QuotesCarousel";
-import Certificates from "@/components/ui/Home/Certificates";
-import SkillsShowcase from "@/components/ui/Home/SkillsShowcase";
-import Workwithme from "@/components/ui/Home/Workwithme";
-import GetInTouch from "@/components/ui/GetInTouch";
-import TextDesign from "@/components/ui/Home/TextDesign";
+// import QuotesCarousel from "@/components/ui/Home/QuotesCarousel";
+// import Certificates from "@/components/ui/Home/Certificates";
+// import SkillsShowcase from "@/components/ui/Home/SkillsShowcase";
+// import Workwithme from "@/components/ui/Home/Workwithme";
+// import GetInTouch from "@/components/ui/GetInTouch";
+// import TextDesign from "@/components/ui/Home/TextDesign";
 import Hero from "@/components/ui/Home/Hero";
 
 export default function Home() {
   return (
     <motion.div className="bg-transparent max-w-7xl mx-auto space-y-32 md:space-y-64">
       <Hero />
-      <TextDesign />
-      <Workwithme />
-      <SkillsShowcase />
-      <Certificates />
-      <QuotesCarousel />
-      <GetInTouch />
+      {/* <TextDesign /> */}
+      {/* <Workwithme /> */}
+      {/* <SkillsShowcase /> */}
+      {/* <Certificates /> */}
+      {/* <QuotesCarousel /> */}
+      {/* <GetInTouch /> */}
     </motion.div>
   );
 }
