9d51ab5906e1fc5e0b026976a86b7686285da1dd
Ishant Kumar <ishant010301@gmail.com>
2025-03-12 01:42:01 +0530

    check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,12 @@ import SkillsShowcase from "@/components/ui/Home/SkillsShowcase";
 import Workwithme from "@/components/ui/Home/Workwithme";
 import GetInTouch from "@/components/ui/GetInTouch";
 import TextDesign from "@/components/ui/Home/TextDesign";
-import Hero from "@/components/ui/Home/Hero";
+// import Hero from "@/components/ui/Home/Hero";
 
 export default function Home() {
   return (
     <motion.div className="bg-transparent max-w-7xl mx-auto space-y-32 md:space-y-64">
-      <Hero />
+      {/* <Hero /> */}
       <TextDesign />
       <Workwithme />
       <SkillsShowcase />
