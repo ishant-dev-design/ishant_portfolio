21a08fa0486c0cf16cbd55a114ca6425c4dd6d46
Ishant Kumar <ishant010301@gmail.com>
2025-03-12 02:52:15 +0530

    Update Hero.tsx

--- src/components/ui/Home/Hero.tsx
+++ src/components/ui/Home/Hero.tsx
@@ -53,12 +53,13 @@ const Hero = () => {
               <div className="w-full h-full">
                 <iframe
                   className="w-full !rounded-2xl"
-                  style={{ border: "none", height: "400px" }} // Adjust height as needed
-                  src="https://open.spotify.com/embed/playlist/37i9dQZEVXcNheyb00KEzN?utm_source=generator"
+                  style={{ border: "none", height: "400px" }}
+                  src="https://open.spotify.com/embed/playlist/5ot1X8S8pP5B9luUISqD7n?utm_source=generator"
                   width="100%"
                   frameBorder="0"
                   allowFullScreen
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  loading="lazy"
                 />
               </div>
             </div>
