e0857a57eaa485c4700e49a1238e5149fd825877
Ishant Kumar <ishant010301@gmail.com>
2025-03-12 01:01:38 +0530

    check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import QuotesCarousel from "@/components/ui/Home/QuotesCarousel";
 import Certificates from "@/components/ui/Home/Certificates";
 import SkillsShowcase from "@/components/ui/Home/SkillsShowcase";
-// import Workwithme from "@/components/ui/Home/Workwithme";
+import Workwithme from "@/components/ui/Home/Workwithme";
 import GetInTouch from "@/components/ui/GetInTouch";
 // import TextDesign from "@/components/ui/Home/TextDesign";
 // import Hero from "@/components/ui/Home/Hero";
@@ -13,7 +13,7 @@ export default function Home() {
     <motion.div className="bg-transparent max-w-7xl mx-auto space-y-32 md:space-y-64">
       {/* <Hero /> */}
       {/* <TextDesign /> */}
-      {/* <Workwithme /> */}
+      <Workwithme />
       <SkillsShowcase />
       <Certificates />
       <QuotesCarousel />
