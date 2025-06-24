ee64e60420ef7a8c15006c72a6161594d503bd93
Ishant Kumar <ishant010301@gmail.com>
2025-06-24 21:13:49 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "next-themes";
 import Squares from "./Square_BG";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const pathname = usePathname();
 
   useEffect(() => {
